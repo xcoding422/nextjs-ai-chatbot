7120dcff3e1b3704e41e9702e6c6d5e33e61f922
xcoding422 <20662920@qq.com>
2025-03-29 15:03:12 +0800

    Update overview.tsx

--- components/overview.tsx
+++ components/overview.tsx
@@ -6,7 +6,8 @@ import { MessageIcon, VercelIcon } from './icons';
 export const Overview = () => {
   return (
     <motion.div>
-        <p><center>我是Deepseek, 快来与我聊天吧!</center></p>
+      <p><center><h1>我是 DeepSeek, 很高兴见到你!</h1></center></p>
+      <p><center>我可以帮你写代码/读文件/写作各种创意内容, 请把你的任务交给我吧~</center></p>
     </motion.div>
   );
 };
