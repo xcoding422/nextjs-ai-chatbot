293ad9e829c8f221815b5111ea64be282c5c545b
xcoding422 <20662920@qq.com>
2025-03-29 10:34:15 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import './globals.css';
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://chat.vercel.ai'),
-  title: 'Next.js Chatbot Template',
-  description: 'Next.js chatbot template using the AI SDK.',
+  title: 'AI在线聊天',
+  description: '由Deepseek大模型驱动.',
 };
 
 export const viewport = {
