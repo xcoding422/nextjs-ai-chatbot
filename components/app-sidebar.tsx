0fb25af87cc29fc20ef2a40d63802326257a7e2a
xcoding422 <20662920@qq.com>
2025-03-29 10:52:59 +0800

    Update app-sidebar.tsx

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -35,7 +35,7 @@ export function AppSidebar({ user }: { user: User | undefined }) {
               className="flex flex-row gap-3 items-center"
             >
               <span className="text-lg font-semibold px-2 hover:bg-muted rounded-md cursor-pointer">
-                Chatbot
+                聊天机器人
               </span>
             </Link>
             <Tooltip>
@@ -53,7 +53,7 @@ export function AppSidebar({ user }: { user: User | undefined }) {
                   <PlusIcon />
                 </Button>
               </TooltipTrigger>
-              <TooltipContent align="end">New Chat</TooltipContent>
+              <TooltipContent align="end">新聊天</TooltipContent>
             </Tooltip>
           </div>
         </SidebarMenu>
